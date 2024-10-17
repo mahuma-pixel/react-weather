5ab6703f24fdf823e20c9c792312661189d352e6
mahuma-pixel <69123574+mahuma-pixel@users.noreply.github.com>
2024-10-17 10:32:42 +0200

    removed app

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import Weather from './weather';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
     <Weather/>
   </React.StrictMode>
 );
